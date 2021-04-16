c2cbd0b76e9bd534ba40018f9884649069aedde6
Bryson2809 <70281849+Bryson2809@users.noreply.github.com>
2021-04-15 22:33:13 -0400

    Add files via upload

--- task2.c
+++ task2.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
+#include <unistd.h>
 
 typedef struct {
 	int id_num; //id
@@ -29,6 +30,7 @@ int southBats = 0;
 int westBats = 0;
 int eastBats = 0;
 
+//Condition variables
 pthread_cond_t eastQueue;
 pthread_cond_t southQueue;
 pthread_cond_t westQueue;
@@ -39,14 +41,16 @@ pthread_cond_t southFirst;
 pthread_cond_t westFirst;
 pthread_cond_t northFirst;
 
-void *goBatGo(void *arg);
-void doTheRest(void *arg);
-void *arrive(void *arg);
-void *cross(void *arg);
-void *leave(void *arg);
-void *check(void *arg);
+//Function Declerations
+void *goBatGo(bat b);
+void *doTheRest(bat b);
+void *arrive(bat b);
+void *cross(bat b);
+void *leave(bat b);
+void *check(bat b);
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
 	//Let's safely assume that we will at most have 
 	//100 BATs
 	char directions[100]; 
@@ -60,13 +64,13 @@ int main(int argc, char **argv){
 		bat temp;
 		temp.id_num = i;
 		temp.direct = directions[i];
-		if (temp.direct = 'w')
+		if (temp.direct == 'w')
 			temp.right = 's';
-		else if (temp.direct = 'n')
+		else if (temp.direct == 'n')
 			temp.right = 'w';
-		else if (temp.direct = 'e')
+		else if (temp.direct == 'e')
 			temp.right = 'n';
-		else if (temp.direct = 's')
+		else if (temp.direct == 's')
 			temp.right = 'e';
 		bats[i] = temp;
 	}
@@ -77,51 +81,59 @@ int main(int argc, char **argv){
 	// maybe send to a function that takes in a 
 	// bat struct... 
 	for(int i = 0; i < (sizeof(bats) / sizeof(bats[0])); i++)
-		printf("bats\n");
-	
+    {
+        goBatGo(bats[i]);
+    }
 	return 0; 
 }
 
-void *goBatGo(void *arg){
-	printf("goBatGoRuns");
-	bat *b = (bat *) arg; 
+void *goBatGo(bat b)
+{
 	check(b);
 	doTheRest(b);
 }
 
-void doTheRest(void *arg){
-	printf("DTR runs");
-	bat *b = (bat *) arg;
+void *doTheRest(bat b)
+{
 	arrive(b);
 	cross(b);
 	leave(b);
 }
 
-void *arrive(void *arg)
+/**
+ * This function handles the bats arriving and should casue them to wait at the crossroads but they move on imediately instead
+ **/
+void *arrive(bat b)
 {
-	printf("arrive runs");
-	bat *b = (bat *) arg; 
 	pthread_mutex_lock(&mutex);
-	if (b->direct == 'n') 
+	if (b.direct == 'n') 
 	{ 
+        printf("BAT %d", b.id_num);
+        printf(" from North arrives at crossing\n");
 		northBats++;
 	    if (northBats > 1)
 	       pthread_cond_wait(&northQueue, &mutex);
 	}
-	else if (b->direct == 'e')
+	else if (b.direct == 'e')
 	{
+        printf("BAT %d", b.id_num);
+        printf(" from East arrives at crossing\n");
 		eastBats++;
 		if (eastBats > 1)
 			pthread_cond_wait(&eastQueue, &mutex);
 	}
-	else if (b->direct == 's')
+	else if (b.direct == 's')
 	{
+        printf("BAT %d", b.id_num);
+        printf(" from South arrives at crossing\n");
 		southBats++;
 		if (southBats > 1)
 			pthread_cond_wait(&southQueue, &mutex);
 	}
-	else if (b->direct == 'w')
+	else if (b.direct == 'w')
 	{
+        printf("BAT %d", b.id_num);
+        printf(" from West arrives at crossing\n");
 		westBats++;
 		if (westBats > 1)
 			pthread_cond_wait(&westQueue, &mutex);
@@ -131,79 +143,119 @@ void *arrive(void *arg)
 	pthread_mutex_unlock(&mutex);
 }
 
-void *cross(void *arg)
+/**
+ * I beleive that the issue with my code is in this function. 
+ * I am not able to figure out how to get the bats to wait at
+ * the crossroads and they are instead leaving as soon as they arrive.
+**/
+void *cross(bat b)
 {
-	printf("cross runs");
-	bat *b = (bat *) arg; 
+    sleep(1);
 	pthread_mutex_lock(&mutex);
-	if (b->right == 'n' && northBats > 0) 
+	if (b.right == 'n' && northBats > 0) 
+    {
 	    pthread_cond_wait(&northFirst, &mutex); 
-	else if (b->right == 'e' && eastBats > 0)
+    }
+	else if (b.right == 'e' && eastBats > 0)
+    {
 		pthread_cond_wait(&eastFirst, &mutex);
-	else if (b->right == 's' && southBats > 0)
+    }
+	else if (b.right == 's' && southBats > 0)
+    {
 		pthread_cond_wait(&southFirst, &mutex);
-	else if (b->right == 'w' && westBats > 0)
+    }
+	else if (b.right == 'w' && westBats > 0)
+    {
 		pthread_cond_wait(&westFirst, &mutex);
-	
-	if (b->direct == 'n')
+    }
+
+	if (b.direct == 'n')
 	{
 		northBats--;
 		pthread_cond_signal(&northQueue);
 	}
-	else if (b->direct == 'e')
+	else if (b.direct == 'e')
 	{
 		eastBats--;
 		pthread_cond_signal(&eastQueue);
 	}
-	else if (b->direct == 's')
+	else if (b.direct == 's')
 	{
 		southBats--;
 		pthread_cond_signal(&southQueue);
 	}
-	else if (b->direct == 'w')
+	else if (b.direct == 'w')
 	{
 		westBats--;
 		pthread_cond_signal(&westQueue);
 	}
-
 	total--; 
 	pthread_mutex_unlock(&mutex);
 }
 
-void *leave(void *arg)
+/**
+ * This function is responsible for making the bats leave the crossroad when it is their time
+ **/
+void *leave(bat b)
 {
-	printf("leave runs");
-	bat *b = (bat *) arg;
 	pthread_mutex_lock(&mutex);
-	if (b->direct == 'n')
-	    pthread_cond_signal(&northFirst);
-	else if (b->direct == 'e')
-		pthread_cond_signal(&eastFirst);
-	else if (b->direct == 's')
-		pthread_cond_signal(&southFirst);
-	else if (b->direct == 'w')
-		pthread_cond_signal(&westFirst);
-	
+	if (b.direct == 'n')
+    {
+        pthread_cond_signal(&northFirst);
+        printf("BAT %d", b.id_num);
+        printf(" from North leaves the crossing\n");
+    }   
+	else if (b.direct == 'e')
+    {
+        pthread_cond_signal(&eastFirst);
+        printf("BAT %d", b.id_num);
+        printf(" from East leaves the crossing\n");
+    }
+	else if (b.direct == 's')
+    {
+        pthread_cond_signal(&southFirst);
+        printf("BAT %d", b.id_num);
+        printf(" from South leaves the crossing\n");
+    }
+	else if (b.direct == 'w')
+    {
+        pthread_cond_signal(&westFirst);
+        printf("BAT %d", b.id_num);
+        printf(" from West leaves the crossing\n");
+    }
 	pthread_mutex_unlock(&mutex);
 }
-void *check(void *arg)
+
+/**
+ * This function is responsible for checking for deadlock, I am not sure if this one works since I can not get the bats to wait
+ **/
+void *check(bat b)
 {
-	printf("check runs");
 	pthread_mutex_lock(&mutex);
-	bat *b = (bat *) arg;
 	if (total == 4)
 	{
-		if (b->direct == 'n')
-			pthread_cond_signal(&northFirst);
-		else if (b->direct == 'e')
-			pthread_cond_signal(&eastFirst);
-		else if (b->direct == 's')
-			pthread_cond_signal(&southFirst);
-		else if (b->direct == 'w')
-			pthread_cond_signal(&westFirst);
+        printf("BAT jam detected,");
+		if (b.direct == 'n')
+        {
+            pthread_cond_signal(&northFirst);
+            printf(" signaling north to go\n");
+        }	
+		else if (b.direct == 'e')
+        {
+            pthread_cond_signal(&eastFirst);
+            printf(" signaling east to go\n");
+        }
+		else if (b.direct == 's')
+        {
+            pthread_cond_signal(&southFirst);
+            printf(" signaling south to go\n");
+        }
+			
+		else if (b.direct == 'w')
+        {
+            pthread_cond_signal(&westFirst);
+            printf(" singaling west to go\n");
+        }
 	}
 	pthread_mutex_unlock(&mutex);
 }
-
-
-
